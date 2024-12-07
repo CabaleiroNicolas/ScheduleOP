3d54824dbd25169a0015d47a530ae8b1b12c62b8
ivanochoa229 <ivanochoa229@gmail.com>
2024-12-06 21:39:28 -0300

    Se agrego el archivo con valores de prueba en resources, y se corrigio el valor null devuelto por el dto en el courseGroup

--- src/main/java/OptaTest/OptaPlanner/Prueba/entity/ScheduleOptaPlanner.java
+++ src/main/java/OptaTest/OptaPlanner/Prueba/entity/ScheduleOptaPlanner.java
@@ -1,15 +1,12 @@
 package OptaTest.OptaPlanner.Prueba.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
-import org.optaplanner.core.api.domain.lookup.PlanningId;
 
 import java.util.ArrayList;
 
 @AllArgsConstructor
 @NoArgsConstructor
 public class ScheduleOptaPlanner {
-    @JsonProperty("couseGroup")
     private String courseGroup;
 
     private ArrayList<DayAndTimeOptaPlanner> dayAndTimes;
